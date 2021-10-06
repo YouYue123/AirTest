5a2d8f20cfc7ae0408aaf9e6152839c556038b01
YouYue123 <youyue@airsquire.ai>
2021-10-06 11:00:46 +0800

    Fix

--- coverage.py
+++ coverage.py
@@ -7,21 +7,23 @@ import os.path
 from termcolor import colored
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapispip3.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1uPvwCkQmBNqdQOhmoTBEHWrK6aaqiRc4EQa2QVHkiY4'
 
 dict = {}
+
+
 def readData(data):
     for item in data[1:]:
         dict[item[0]] = {
-                        "key": item[0],
-                        "parent": item[3],
-                         "description": item[1], 
-                         "flow": item[2],
-                        "status": item[-1]
-                        }
+            "key": item[0],
+            "parent": item[3],
+            "description": item[1],
+            "flow": item[2],
+            "status": item[-1]
+        }
 
 
 def main():
@@ -48,9 +50,9 @@ def main():
     # Call the Sheets API
     sheet = service.spreadsheets()
     result = sheet.values().batchGet(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                     ranges=["Project", "VirtualSite", "Progress dashboard", 
-                                             "Link", "Markup dashboard", "PDF Report", 
-                                             "Videowalk", "Attachment in VirtualSite", "Markup in VirtualSite", "BIM", 
+                                     ranges=["Project", "VirtualSite", "Progress dashboard",
+                                             "Link", "Markup dashboard", "PDF Report",
+                                             "Videowalk", "Attachment in VirtualSite", "Markup in VirtualSite", "BIM",
                                              "Labels and categories", "Usage", "Project Settings", "Miscellaneous"]).execute()
     list = result["valueRanges"]
     for item in list:
@@ -63,21 +65,23 @@ def main():
     for key in dict.keys():
         item = dict[key]
         if item["status"] == "P" or item["status"] == "p":
-          passedCases += 1
+            passedCases += 1
         elif item["status"] == "F" or item["status"] == "f":
-          failedCases += 1
+            failedCases += 1
         else:
-          unTestedCases += 1
-          unTestedList.append(item)
-    print(colored("Tested cases", "blue"), passedCases + failedCases + unTestedCases)
-    print(colored("Test coverage", "blue"), (passedCases + failedCases) / (passedCases + failedCases + unTestedCases) )
+            unTestedCases += 1
+            unTestedList.append(item)
+    print(colored("Tested cases", "blue"),
+          passedCases + failedCases + unTestedCases)
+    print(colored("Test coverage", "blue"), (passedCases +
+          failedCases) / (passedCases + failedCases + unTestedCases))
     print(colored("Successful: ", "green"), passedCases)
     print(colored("Failed: ", "red"), failedCases)
     print("Untested cases: \n")
     for item in unTestedList:
-      if item["flow"] == "Main Flow":
-        print(colored("warnning, mainflow not tested", "yellow"))
-        print(colored(item["key"], "green"), item["description"])
+        if item["flow"] == "Main Flow":
+            print(colored("warnning, mainflow not tested", "yellow"))
+            print(colored(item["key"], "green"), item["description"])
     print("Done!")
 
 
